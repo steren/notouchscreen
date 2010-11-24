5152bf627feec2ec0f57501d728e719bd6fc600b
canard <canard@myriam.(none)>
2010-11-23 23:57:28 -0800

    Working cmake project.

---
+++ src/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <cv.h>
+#include <cv.hpp>
+#include <highgui.h>
+#include <highgui.hpp>
+
+int main(int argc, char * argv[])
+{
+    std::cout << "Hello World" << std::endl;
+}
+
---
+++ version.cmake
@@ -0,0 +1,10 @@
+# Version
+set (opencvtest_MAJOR 0)
+set (opencvtest_MINOR 0)
+set (opencvtest_PATCH 1)
+
+set (opencvtest_VERSION ${opencvtest_MAJOR}.${opencvtest_MINOR}.${opencvtest_PATCH})
+
+# Release date
+set(opencvtest_RELEASE 2010-11-23)
+
