c1a3601ff1caeee5da5ec8fe806866691cb24a16
Steren <steren.giannini@gmail.com>
2010-12-16 23:36:14 +0100

    Motion Hisroty computation and vizualisation. The Silhouette needs some love

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,11 +5,13 @@
 
 using namespace cv;
 
+const double DURATION = 1;
 
 int main(int argc, char * argv[])
 {
 	cv::VideoCapture cap1(0);
 	cv::namedWindow("Visu", CV_WINDOW_AUTOSIZE);
+	cv::namedWindow("Motion History", CV_WINDOW_AUTOSIZE);
 
 	if(cap1.isOpened())
 	{
@@ -25,7 +27,13 @@ int main(int argc, char * argv[])
 		frames[0] = empty.clone();
 		frames[1] = empty.clone();
 
-		cv::Mat silhouette;
+		cv::Mat silhouette; 	// the silouhette image
+		cv::Mat silhouette2; 	// the silouhette image
+		double timestamp; 		// the timestamp of each silhouette
+
+		cv::Mat mhi(empty.size(), CV_32FC1); // the Motion History image
+		cv::Mat mhiVisu; // a display of the MHI
+
 		for(;;)
 		{
 			nextFrame = (currentFrame+1)%2;
@@ -33,7 +41,17 @@ int main(int argc, char * argv[])
 			frames[currentFrame] = currentCap.clone();
 			currentCap.copyTo(frames[currentFrame]);
 			cv::absdiff(frames[currentFrame],frames[nextFrame],silhouette);
-			cv::imshow("Visu",silhouette);
+			cv::cvtColor( silhouette, silhouette2, CV_BGR2GRAY );
+			cv::imshow("Visu", silhouette2);
+
+			timestamp = (double)clock()/CLOCKS_PER_SEC;
+			cv::updateMotionHistory(silhouette2, mhi, timestamp, DURATION);
+
+			// Display the MHI (need scaling)
+			cv::convertScaleAbs(mhi, mhiVisu, 255./DURATION, (DURATION - timestamp)*255./DURATION );
+			cv::imshow("Motion History", mhiVisu);
+
+
 			if(cv::waitKey(30) >= 0) break;
 			currentFrame = nextFrame;
 		}
